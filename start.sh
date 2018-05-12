090fa25303ba8e0f02587df5580d691fff2e79c9
Christopher Guckes <cg@bdl.de>
2018-05-12 11:54:50 +0200

    Added more sample test cases.

--- start.sh
+++ start.sh
@@ -4,10 +4,17 @@ CLASSPATH="target/dependencies/*"
 JAVA="$(which java)"
 
 FITNESSE_SOURCES="src/main/resources"
+FITNESSE_INIT="/opt/fitnesse-initial"
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   FITNESSE_SOURCES="src\\main\\resources"
 fi
 
-/usr/bin/mkdir -p "${FITNESSE_SOURCES}"
+mkdir -p "${FITNESSE_SOURCES}"
+
+if [ ! -e "${FITNESSE_SOURCES}/FitNesseRoot" ]; then
+  echo "No previous fitnesse installation found, setting up first initial config."
+  cp -r ${FITNESSE_INIT}/* ${FITNESSE_SOURCES}
+fi
+
 "${JAVA}" -cp "${CLASSPATH}" fitnesseMain.FitNesseMain -p ${PORT} -d "${FITNESSE_SOURCES}" -e 0
